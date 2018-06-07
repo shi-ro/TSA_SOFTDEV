f2c4e9595b7afd7e9f5b8996a962e410d3dd6c64
Kuro <body.identity@outlook.com>
2018-06-07 16:12:40 -0700

    added connection

--- TSA_SOFTDEV/TSA_SOFTDEV/Solver.cs
+++ TSA_SOFTDEV/TSA_SOFTDEV/Solver.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace TSA_SOFTDEV
 {
-    public partial class Form1 : Form
+    public partial class Solver : Form
     {
-        public Form1()
+        public Solver()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
