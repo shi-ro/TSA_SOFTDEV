733553d2c7be94fa412f48d076b75f00df8cafaa
Kuro <body.identity@outlook.com>
2018-06-11 19:29:04 -0700

    performed check

--- TSA_SOFTDEV/Core/Server/Integration.cs
+++ TSA_SOFTDEV/Core/Server/Integration.cs
@@ -11,39 +11,7 @@ namespace Core.Server
     {
         private static SqlConnection _connection = new SqlConnection("Server=tcp:softdevserver.database.windows.net,1433;Initial Catalog=SoftDevDB;Persist Security Info=False;User ID=serveradmin;Password=SoftDev!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
-        public static void executeAddUser(User bob)
-        {
-            SqlCommand cmdNew = new SqlCommand("INSERT INTO[dbo].[Users] VALUES('" + bob.getName() + "', '" + bob.getPassword() + "', " + bob.getPoints() + ", '" + bob.getClassrooms() + "', '" + bob.getRanks() + "', " + bob.getTeamId() + ")", _connection);
-            cmdNew.CommandType = CommandType.Text;
-
-            _connection.Open();
-
-            cmdNew.ExecuteNonQuery();
-
-            _connection.Close();
-        }
-
-        public static User executeGetUser(string name) //using a name, get a user
-        {
-            SqlCommand cmdNew = new SqlCommand("SELECT Users.Password, Users.Points, Users.Classrooms, Users.Ranks, Users.TeamId FROM Users where Users.Name = " + name, _connection);
-
-            cmdNew.CommandType = CommandType.Text;
-
-            User userToReturn = null;
-
-            _connection.Open();
-            SqlDataReader reader = cmdNew.ExecuteReader();
-            while(reader.Read())
-            {
-                userToReturn = new User(name, (String)reader[0], (int)reader[1], (String)reader[2], (String)reader[3], (int)reader[4]);
-            }
-            reader.Close();
-            
-
-            return userToReturn; 
-        }
-
-        public static object ExecuteCommand(string command)
+        public static object ExecuteQuery(string command)
         {
             SqlCommand cmdNew = new SqlCommand(command, _connection);
             cmdNew.CommandType = CommandType.Text;
@@ -51,6 +19,51 @@ namespace Core.Server
             var result = cmdNew.ExecuteNonQuery();
             _connection.Close();
             return result;
+        }
+
+        public static SqlDataReader ExecuteRead(string command)
+        {
+            SqlCommand cmdNew = new SqlCommand(command, _connection);
+            cmdNew.CommandType = CommandType.Text;
+            _connection.Open();
+            SqlDataReader reader = cmdNew.ExecuteReader();
+            _connection.Close();
+            return reader;
+        }
+
+        public static void ExecuteAddUser(User bob)
+        {
+
+            // ExecuteQuery($"INSERT INTO[dbo].[Users] VALUES('{bob.getName()}', '{bob.getPassword()}', {bob.getPoints()}, '{bob.getClassrooms()}', '{bob.getRanks()}', {bob.getTeamId()})");
+            // everything below can be replaced with above statement
+
+            SqlCommand cmdNew = new SqlCommand("INSERT INTO[dbo].[Users] VALUES('" + bob.getName() + "', '" + bob.getPassword() + "', " + bob.getPoints() + ", '" + bob.getClassrooms() + "', '" + bob.getRanks() + "', " + bob.getTeamId() + ")", _connection);
+            cmdNew.CommandType = CommandType.Text;
+            _connection.Open();
+            cmdNew.ExecuteNonQuery();
+            _connection.Close();
+        }
+
+        public static User ExecuteGetUser(string name) //using a name, get a user
+        {
+            
+            SqlCommand cmdNew = new SqlCommand("SELECT Users.Password, Users.Points, Users.Classrooms, Users.Ranks, Users.TeamId FROM Users where Users.Name = " + name, _connection);
+            cmdNew.CommandType = CommandType.Text;
+            User userToReturn = null;
+            _connection.Open();
+            SqlDataReader reader = cmdNew.ExecuteReader();
+
+            // above code can be replaced with 
+            // SqlDataReader reader = ExecuteRead($"SELECT Users.Password, Users.Points, Users.Classrooms, Users.Ranks, Users.TeamId FROM Users where Users.Name = {name}");
+
+            while (reader.Read())
+            {
+                userToReturn = new User(name, (String)reader[0], (int)reader[1], (String)reader[2], (String)reader[3], (int)reader[4]);
+            }
+
+            //line below probably needs to be _connection.Close() instead of reader.Close()
+            reader.Close();
+            return userToReturn; 
         }
     }
 }
