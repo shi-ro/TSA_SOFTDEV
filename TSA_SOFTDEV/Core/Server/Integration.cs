a2f344cf9ccb58c694633dd0b8a45e40c56bb579
DragonDude16 <matthewsaveliev@yahoo.com>
2018-06-11 18:07:34 -0700

    added executeGetUser(name) and a User class

--- TSA_SOFTDEV/Core/Server/Integration.cs
+++ TSA_SOFTDEV/Core/Server/Integration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MainMenu;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -9,6 +10,27 @@ namespace Core.Server
     public static class Integration
     {
         private static SqlConnection _connection = new SqlConnection("Server=tcp:softdevserver.database.windows.net,1433;Initial Catalog=SoftDevDB;Persist Security Info=False;User ID=serveradmin;Password=SoftDev!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        public static User executeGetUser(string name) //using a name, get a user
+        {
+            SqlCommand cmdNew = new SqlCommand("SELECT Users.Password, Users.Points, Users.Classrooms, Users.Ranks, Users.TeamId FROM Users where Users.Name = " + name, _connection);
+
+            cmdNew.CommandType = CommandType.Text;
+
+            User userToReturn = null;
+
+            _connection.Open();
+            SqlDataReader reader = cmdNew.ExecuteReader();
+            while(reader.Read())
+            {
+                userToReturn = new User(name, (String)reader[0], (int)reader[1], (String)reader[2], (String)reader[3], (int)reader[4]);
+            }
+            reader.Close();
+            
+
+            return userToReturn; 
+        }
+
         public static object ExecuteCommand(string command)
         {
             SqlCommand cmdNew = new SqlCommand(command, _connection);
---
+++ TSA_SOFTDEV/MainMenu/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMenu
+{
+    public class User
+    {
+        private static String name;
+        private static String password;
+        private static int points;
+        private static String classrooms;
+        private static String ranks;
+        private static int teamId;
+
+        public User(String n, String p, int po, String c, String r, int t)
+        {
+            name = n;
+            password = p;
+            points = po;
+            classrooms = c;
+            ranks = r;
+            teamId = t;
+        }
+    }
+}
