5857e8df754cd1cf515647ce1a269f86ebbbad90
Kuro <body.identity@outlook.com>
2018-06-22 13:50:01 -0400

    .

--- TSA_SOFTDEV/MainMenu/LoginScreen.cs
+++ TSA_SOFTDEV/MainMenu/LoginScreen.cs
@@ -29,7 +29,7 @@ namespace MainMenu
 
         private void LoginScreen_Load(object sender, EventArgs e)
         {
-            Core.Server.Integration.ExecuteAddProblemset("SQL DAT TEST",5,1,"_-_","","-1,10","Testing SQL server problemset addition ( but its subtraction )");
+            //Core.Server.Integration.ExecuteAddProblemset("SQL DAT TEST",5,1,"_-_","","-1,10","Testing SQL server problemset addition ( but its subtraction )");
         }
 
         private void TryLogin()
