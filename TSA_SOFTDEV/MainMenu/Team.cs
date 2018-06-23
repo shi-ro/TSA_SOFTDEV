4a9ddf34a5182ed11944512194614f8d31c07189
Kuro <body.identity@outlook.com>
2018-06-23 15:22:30 -0400

    .

--- TSA_SOFTDEV/MainMenu/Team.cs
+++ TSA_SOFTDEV/MainMenu/Team.cs
@@ -9,15 +9,13 @@ namespace MainMenu
     public class Team
     {
         public String Name { get; set; }
-        public int Points { get; set; }
         public int Id { get; set; }
         public String Users { get; set; }
 
-        public Team(String n, int po, String u, int i)
+        public Team(String n, String u, int i)
         {
             Name = n;
             Users = u;
-            Points = po;
             Id = i;
         }
     }
